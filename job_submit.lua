5609b2ef8be3236a8463cb258ffb0f3b712da0b9
Rémi Palancher <remi@rackslab.io>
2026-06-12 15:55:37 +0200

    chore: re-license under GPLv2+
    fix #45

--- job_submit.lua
+++ job_submit.lua
@@ -3,7 +3,8 @@
 Slurm Quota Job Submit Script
 
 Copyright (c) 2025 Rackslab
-SPDX-License-Identifier: MIT
+Copyright (c) 2025 Université de Montpellier
+SPDX-License-Identifier: GPL-2.0-or-later
 
 This script is designed to be used as a Slurm job_submit plugin to enforce
 users & accounts CPU minute quotas before job submission or modification.
